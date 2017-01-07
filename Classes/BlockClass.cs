e3df8eeb5665c6a4fca9d79290f2368ddbe6bd34
YinjiDawn <kenny.on@gmail.com>
2017-01-07 23:14:26 +0200

    add blockclass (consists of different types: building, terrain)

---
+++ Classes/BlockClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RiseOfStrongholds.Classes
+{
+    class BlockClass
+    {
+        /* VARIABLES */
+        private string m_block_name;
+        private string m_block_type; // Should be TypeClass in future
+        private CharacterClass m_block_owner;
+        private StatsClass m_stats;
+        private GameTimeClass m_birthDate;
+
+        /*GET & SET*/
+        public string getBlockName() { return m_block_name; }
+        public string getBlockType() { return m_block_type; }
+        public CharacterClass getBlockOwner() { return m_block_owner; }
+        public StatsClass getStats() { return m_stats; }
+        public GameTimeClass getBirthDate() { return m_birthDate; }
+
+        public void setBlockName(string blockName) { m_block_name = blockName; }
+        public void setBlockType(string blockType) { m_block_type = blockType; }
+        public void setBlockOwner(CharacterClass character) { m_block_owner = character; }
+
+        /*CONSTRUCTORS*/
+        public BlockClass()
+        {
+            /*DEBUG HIGH*/
+            if (ConstantClass.DEBUG_LOG_LEVEL == ConstantClass.DEBUG_LEVELS.HIGH) { ConstantClass.LOGGER.writeToDebugLog("\t->BlockClass()"); };
+
+            m_block_name = "";
+            m_block_type = "";
+            m_stats = new StatsClass();
+            m_birthDate = new GameTimeClass(ConstantClass.gameTime);
+            m_block_owner = new CharacterClass();
+
+            /*DEBUG HIGH*/
+            if (ConstantClass.DEBUG_LOG_LEVEL == ConstantClass.DEBUG_LEVELS.HIGH) { ConstantClass.LOGGER.writeToDebugLog("\t<-BlockClass()"); };
+        }
+    }
+}
