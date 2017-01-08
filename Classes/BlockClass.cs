d6d36918c057186bfdf130fada03a9bb82e122a5
YinjiDawn <kenny.on@gmail.com>
2017-01-08 23:02:31 +0200

    Update block owner to Guid

--- Classes/BlockClass.cs
+++ Classes/BlockClass.cs
@@ -11,20 +11,20 @@ namespace RiseOfStrongholds.Classes
         /* VARIABLES */
         private string m_block_name;
         private string m_block_type; // Should be TypeClass in future
-        private CharacterClass m_block_owner;
+        private Guid m_unique_character_id;
         private StatsClass m_stats;
         private GameTimeClass m_birthDate;
 
         /*GET & SET*/
         public string getBlockName() { return m_block_name; }
         public string getBlockType() { return m_block_type; }
-        public CharacterClass getBlockOwner() { return m_block_owner; }
+        public Guid getBlockOwner() { return m_unique_character_id; }
         public StatsClass getStats() { return m_stats; }
         public GameTimeClass getBirthDate() { return m_birthDate; }
 
         public void setBlockName(string blockName) { m_block_name = blockName; }
         public void setBlockType(string blockType) { m_block_type = blockType; }
-        public void setBlockOwner(CharacterClass character) { m_block_owner = character; }
+        public void setBlockOwner(Guid character_id) { m_unique_character_id = character_id; }
 
         /*CONSTRUCTORS*/
         public BlockClass()
@@ -36,7 +36,7 @@ namespace RiseOfStrongholds.Classes
             m_block_type = "";
             m_stats = new StatsClass();
             m_birthDate = new GameTimeClass(ConstantClass.gameTime);
-            m_block_owner = new CharacterClass();
+            m_unique_character_id = new Guid();
 
             /*DEBUG HIGH*/
             if (ConstantClass.DEBUG_LOG_LEVEL == ConstantClass.DEBUG_LEVELS.HIGH) { ConstantClass.LOGGER.writeToDebugLog("\t<-BlockClass()"); };
